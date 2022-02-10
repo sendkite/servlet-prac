f141f749dcdf3f906b1930997ac022b056839d2c
sendkite <sendkite1@gmail.com>
2022-02-10 16:38:22 +0900

    Feature : 과거 스프링 HandlerMapping, HandlerAdapter 사용 방식

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") // 스프링빈의 이름 url로 준거, handlerMapping에서 이 컨크롤러를 찾음 (by 스프링 빈의 이름으로 찾음 , 최우선으로는 @RequestMapping으로 찾음)
+public class OldController implements Controller { // controller가 실행할 수 있는 인터페이스인지 스프링 핸들러 어댑터가 확인
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
